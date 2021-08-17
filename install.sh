53a347582f41723a9a2d851f45c5ce02fb11dbb8
senowahyu62 <70935444+senowahyu62@users.noreply.github.com>
2021-08-17 13:54:46 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ plain='\033[0m'
 cur_dir=$(pwd)
 
 # check root
-[[ $EUID -ne 0 ]] && echo -e "${red}错误：${plain} 必须使用root用户运行此脚本！\n" && exit 1
+[[ $EUID -ne 0 ]] && echo -e "${red} error: ${plain} harus menJadi root user untuk menjalankan skrip ini!\n" && exit 1
 
 # check os
 if [[ -f /etc/redhat-release ]]; then
@@ -26,7 +26,7 @@ elif cat /proc/version | grep -Eqi "ubuntu"; then
 elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
     release="centos"
 else
-    echo -e "${red}未检测到系统版本，请联系脚本作者！${plain}\n" && exit 1
+    echo -e "Versi sistem ${red} tidak didukung${plain}\n" && exit 1
 fi
 
 os_version=""
@@ -41,15 +41,15 @@ fi
 
 if [[ x"${release}" == x"centos" ]]; then
     if [[ ${os_version} -le 6 ]]; then
-        echo -e "${red}请使用 CentOS 7 或更高版本的系统！${plain}\n" && exit 1
+        echo -e "${red}Silakan gunakan sistem versi CentOS 7 atau lebih tinggi! ${plain}\n" && exit 1
     fi
 elif [[ x"${release}" == x"ubuntu" ]]; then
     if [[ ${os_version} -lt 16 ]]; then
-        echo -e "${red}请使用 Ubuntu 16 或更高版本的系统！${plain}\n" && exit 1
+        echo -e "${red}Silakan gunakan sistem versi Ubuntu 16 atau lebih tinggi! ${plain}\n" && exit 1
     fi
 elif [[ x"${release}" == x"debian" ]]; then
     if [[ ${os_version} -lt 8 ]]; then
-        echo -e "${red}请使用 Debian 8 或更高版本的系统！${plain}\n" && exit 1
+        echo -e "${red}Silakan gunakan sistem versi Debian 8 atau lebih tinggi! ${plain}\n" && exit 1
     fi
 fi
 
@@ -62,10 +62,10 @@ install_base() {
 }
 
 install_v2ray() {
-    echo -e "${green}开始安装or升级v2ray${plain}"
+    echo -e "${green} mulai menginstal V2ray${plain}"
     bash <(curl -L -s https://install.direct/go.sh)
     if [[ $? -ne 0 ]]; then
-        echo -e "${red}v2ray安装或升级失败，请检查错误信息${plain}"
+        echo -e "${red}pemasangan v2ray gagal, harap periksa log ${plain}"
         exit 1
     fi
     systemctl enable v2ray
@@ -92,13 +92,7 @@ install_v2-ui() {
     if [[ -e /usr/local/v2-ui/ ]]; then
         rm /usr/local/v2-ui/ -rf
     fi
-    last_version=$(curl -Ls "https://api.github.com/repos/sprov065/v2-ui/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-    echo -e "检测到v2-ui最新版本：${last_version}，开始安装"
     wget -N --no-check-certificate -O /usr/local/v2-ui-linux.tar.gz https://github.com/sprov065/v2-ui/releases/download/${last_version}/v2-ui-linux.tar.gz
-    if [[ $? -ne 0 ]]; then
-        echo -e "${red}下载v2-ui失败，请确保你的服务器能够下载Github的文件，如果多次安装失败，请参考手动安装教程${plain}"
-        exit 1
-    fi
     tar zxvf v2-ui-linux.tar.gz
     rm v2-ui-linux.tar.gz -f
     cd v2-ui
@@ -107,32 +101,31 @@ install_v2-ui() {
     systemctl daemon-reload
     systemctl enable v2-ui
     systemctl start v2-ui
-    echo -e "${green}v2-ui v${last_version}${plain} 安装完成，面板已启动，"
-    echo -e ""
-    echo -e "如果是全新安装，默认网页端口为 ${green}65432${plain}，用户名和密码默认都是 ${green}admin${plain}"
-    echo -e "请自行确保此端口没有被其他程序占用，${yellow}并且确保 65432 端口已放行${plain}"
-    echo -e ""
-    echo -e "如果是更新面板，则按你之前的方式访问面板"
-    echo -e ""
-    curl -o /usr/bin/v2-ui -Ls https://raw.githubusercontent.com/sprov065/v2-ui/master/v2-ui.sh
+	echo -e "Instalasi selesai, panel telah diaktifkan,"
+    gema -e ""
+    echo -e "Jika ini adalah instalasi baru, port web default adalah ${green}65432${plain}, dan nama pengguna dan sandi keduanya ${green}admin${plain} secara default.
+    echo -e "${yellow}Pastikan port ini tidak dipakai program lain${plain}"
+    gema -e ""
+    gema -e ""
+    curl -o /usr/bin/v2-ui -Ls https://raw.githubusercontent.com/senowahyu62/v2-ui/master/v2-ui.sh
     chmod +x /usr/bin/v2-ui
-    echo -e "v2-ui 管理脚本使用方法: "
-    echo -e "----------------------------------------------"
-    echo -e "v2-ui              - 显示管理菜单 (功能更多)"
-    echo -e "v2-ui start        - 启动 v2-ui 面板"
-    echo -e "v2-ui stop         - 停止 v2-ui 面板"
-    echo -e "v2-ui restart      - 重启 v2-ui 面板"
-    echo -e "v2-ui status       - 查看 v2-ui 状态"
-    echo -e "v2-ui enable       - 设置 v2-ui 开机自启"
-    echo -e "v2-ui disable      - 取消 v2-ui 开机自启"
-    echo -e "v2-ui log          - 查看 v2-ui 日志"
-    echo -e "v2-ui update       - 更新 v2-ui 面板"
-    echo -e "v2-ui install      - 安装 v2-ui 面板"
-    echo -e "v2-ui uninstall    - 卸载 v2-ui 面板"
-    echo -e "----------------------------------------------"
+    echo -e "metode penggunaan skrip manajemen v2-ui: "
+    echo -e "----------------------------------------------------------- "
+    echo -e "v2-ui 				- show menu manajemen (fungsi lainnya)"
+    echo -e "v2-ui star 		- start panel v2-ui"
+    echo -e "v2-ui stop			- stop panel v2-ui"
+    echo -e "v2-ui restart		- restart panel v2-ui"
+    echo -e "v2-ui status		- lihat status v2-ui"
+    echo -e "v2-ui aktifkan		- set v2-ui untuk memulai secara otomatis setelah booting"
+    echo -e "v2-ui nonaktifkan	- batalkan boot v2-ui dari awal"
+    echo -e "v2-ui log			- lihat log v2-ui"
+    echo -e "v2-ui update		- update panel v2-ui"
+    echo -e "v2-ui install		- install panel v2-ui"
+    echo -e "v2-ui uninstall	- uninstall panel v2-ui"
+    echo -e "----------------------------------------------------------- "
 }
 
-echo -e "${green}开始安装${plain}"
+echo -e "${green}mulai menginstal${plain}"
 install_base
 install_v2ray
 install_v2-ui
